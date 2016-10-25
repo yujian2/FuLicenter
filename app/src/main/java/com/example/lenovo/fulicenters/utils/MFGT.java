658d86fb7893ead40b720e29258b0ad693195b4d
刘宁 <1039212799@qq.com>
2016-10-25 11:57:27 +0800

    添加闪屏工具并使用

---
+++ app/src/main/java/com/example/lenovo/fulicenters/utils/MFGT.java
@@ -0,0 +1,24 @@
+package com.example.lenovo.fulicenters.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.lenovo.fulicenters.R;
+import com.example.lenovo.fulicenters.activity.MainActivity;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+}
