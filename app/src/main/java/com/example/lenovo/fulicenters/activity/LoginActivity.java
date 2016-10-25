16176afcffcac347db442a7901747e87ecf099fb
刘宁 <1039212799@qq.com>
2016-10-25 20:08:21 +0800

    添加登录界面并且没有用户跳转个人中心界面

--- app/src/main/java/com/example/lenovo/fulicenters/activity/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.example.lenovo.fulicenters.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.lenovo.fulicenters.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.username)
+    EditText username;
+    @BindView(R.id.password)
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -25,5 +38,15 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void setListener() {
 
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                break;
+            case R.id.btn_register:
+                break;
+        }
     }
 }
