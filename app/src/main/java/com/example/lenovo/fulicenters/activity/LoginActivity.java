d5a3888b10762db2f4b54f4a92bb91d3961416be
刘宁 <1039212799@qq.com>
2016-10-26 03:19:13 +0800

    完成登录

--- app/src/main/java/com/example/lenovo/fulicenters/activity/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/LoginActivity.java
@@ -1,30 +1,45 @@
 package com.example.lenovo.fulicenters.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.lenovo.fulicenters.I;
 import com.example.lenovo.fulicenters.R;
+import com.example.lenovo.fulicenters.bean.Result;
+import com.example.lenovo.fulicenters.bean.User;
+import com.example.lenovo.fulicenters.net.NetDao;
+import com.example.lenovo.fulicenters.net.OkHttpUtils;
+import com.example.lenovo.fulicenters.utils.CommonUtils;
 import com.example.lenovo.fulicenters.utils.L;
 import com.example.lenovo.fulicenters.utils.MFGT;
+import com.example.lenovo.fulicenters.utils.ResultUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+
 public class LoginActivity extends BaseActivity {
+    private static final  String TAG=LoginActivity.class.getSimpleName();
+
 
     @BindView(R.id.username)
     EditText mUsername;
     @BindView(R.id.password)
     EditText mPassword;
+    String username;
+    String password;
+    LoginActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mContext=this;
         super.onCreate(savedInstanceState);
     }
 
@@ -47,11 +62,70 @@ public class LoginActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
+                chenckedInput();
                 break;
             case R.id.btn_register:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void chenckedInput() {
+       username=mUsername.getText().toString().trim();password=mPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)){
+            CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
+            mUsername.requestFocus();
+            return;
+        }else if (TextUtils.isEmpty(password)){
+            CommonUtils.showShortToast(R.string.password_connot_be_empty);
+            mPassword.requestFocus();
+            return;
+        }
+        login();
+    }
+
+    private void login() {
+       final ProgressDialog pd= new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.Logining));
+        NetDao.login(mContext, username, password, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                Result result = ResultUtils.getResultFromJson(s, User.class);
+
+                L.e(TAG,"result"+s);
+              if (result==null){
+                  CommonUtils.showShortToast(R.string.login_fail);
+              } else {
+                  if (result.isRetMsg()){
+                      User user = (User) result.getRetData();
+                      L.e(TAG,"user="+user);
+                      MFGT.finish(mContext);
+                  }else {
+                      if (result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
+                          CommonUtils.showShortToast(R.string.login_fail);
+
+
+                      }else if (result.getRetCode()==I.MSG_LOGIN_ERROR_PASSWORD){
+                          CommonUtils.showShortToast(R.string.login_fail_error_password);
+
+                      }else {
+
+                          CommonUtils.showShortToast(R.string.login_fail);
+
+                      }
+
+                  }
+              }
+                pd.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showShortToast(error);
+                L.e(TAG,"error" +error);
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/lenovo/fulicenters/bean/User.java
@@ -0,0 +1,96 @@
+package com.example.lenovo.fulicenters.bean;
+
+/**
+ * Created by clawpo on 2016/10/21.
+ */
+
+public class User {
+
+    /**
+     * muserName : a952700
+     * muserNick : 士大夫
+     * mavatarId : 72
+     * mavatarPath : user_avatar
+     * mavatarSuffix : .jpg
+     * mavatarType : 0
+     * mavatarLastUpdateTime : 1476262984280
+     */
+
+    private String muserName;
+    private String muserNick;
+    private int mavatarId;
+    private String mavatarPath;
+    private String mavatarSuffix;
+    private int mavatarType;
+    private String mavatarLastUpdateTime;
+
+
+    public String getMuserName() {
+        return muserName;
+    }
+
+    public void setMuserName(String muserName) {
+        this.muserName = muserName;
+    }
+
+    public String getMuserNick() {
+        return muserNick;
+    }
+
+    public void setMuserNick(String muserNick) {
+        this.muserNick = muserNick;
+    }
+
+    public int getMavatarId() {
+        return mavatarId;
+    }
+
+    public void setMavatarId(int mavatarId) {
+        this.mavatarId = mavatarId;
+    }
+
+    public String getMavatarPath() {
+        return mavatarPath;
+    }
+
+    public void setMavatarPath(String mavatarPath) {
+        this.mavatarPath = mavatarPath;
+    }
+
+    public String getMavatarSuffix() {
+        return mavatarSuffix;
+    }
+
+    public void setMavatarSuffix(String mavatarSuffix) {
+        this.mavatarSuffix = mavatarSuffix;
+    }
+
+    public int getMavatarType() {
+        return mavatarType;
+    }
+
+    public void setMavatarType(int mavatarType) {
+        this.mavatarType = mavatarType;
+    }
+
+    public String getMavatarLastUpdateTime() {
+        return mavatarLastUpdateTime;
+    }
+
+    public void setMavatarLastUpdateTime(String mavatarLastUpdateTime) {
+        this.mavatarLastUpdateTime = mavatarLastUpdateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "muserName='" + muserName + '\'' +
+                ", muserNick='" + muserNick + '\'' +
+                ", mavatarId=" + mavatarId +
+                ", mavatarPath='" + mavatarPath + '\'' +
+                ", mavatarSuffix='" + mavatarSuffix + '\'' +
+                ", mavatarType=" + mavatarType +
+                ", mavatarLastUpdateTime='" + mavatarLastUpdateTime + '\'' +
+                '}';
+    }
+}
