b6c5458ce2073bc706d00558ee5c1e5cd7319731
刘宁 <1039212799@qq.com>
2016-10-25 12:09:05 +0800

    添加底部菜单和butterknife依赖

--- app/src/main/java/com/example/lenovo/fulicenters/activity/MainActivity.java
+++ app/src/main/java/com/example/lenovo/fulicenters/activity/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.lenovo.fulicenters.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.lenovo.fulicenters.R;
 import com.example.lenovo.fulicenters.utils.L;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        L.d("ddfgfh");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("MainActivity onCreate");
+    }
+
+    public void onCheckedChange(View v){
+
     }
 }
