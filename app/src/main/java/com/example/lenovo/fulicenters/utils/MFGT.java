789bcf59d8835db45bf8ba81e06bd65009349f95
刘宁 <1039212799@qq.com>
2016-10-26 12:10:02 +0800

    完成登陆后跳转到个人中心

--- app/src/main/java/com/example/lenovo/fulicenters/utils/MFGT.java
+++ app/src/main/java/com/example/lenovo/fulicenters/utils/MFGT.java
@@ -63,7 +63,9 @@ public class MFGT {
     }
 
     public static void gotoLogin(Activity context){
-        startActivity(context, LoginActivity.class);
+        Intent intent = new Intent();
+        intent.setClass(context,LoginActivity.class);
+        startActivityForResult(context,intent,I.REQUEST_CODE_LOGIN);
     }
 
     public static void gotoRegister(Activity context){
